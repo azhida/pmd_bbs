2735ada12d75dbbc3df911fac29dbc53694b83b4
azhida <wghzhida@126.com>
2019-03-03 17:13:21 +0000

    帖子详情展示页

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title', '坡马代论坛')</title>
+    <meta name="description" content="@yield('description', '坡马代论坛')" />
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
