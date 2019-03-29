2d30db71e1c9da8e32a7f45da87985b2bed2148b
azhida <wghzhida@126.com>
2019-03-29 07:56:37 +0000

    添加 jwt

---
+++ app/Http/Controllers/Api/TestCodesController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class TestCodesController extends Controller
+{
+    public function test1()
+    {
+        return response()->json(['a' => 1, 'b' => 2]);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,9 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Support\Facades\Auth;
 use Spatie\Permission\Traits\HasRoles;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Traits\LastActivedAtHelper;
     use Traits\ActiveUserHelper;
@@ -90,4 +91,13 @@ class User extends Authenticatable
         $this->unreadNotifications->markAsRead();
     }
 
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
